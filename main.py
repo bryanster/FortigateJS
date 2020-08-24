4ddf00e5c6fd5b8504159dce384d149bbb453698
Bryan van der net <bryan@pop-os.localdomain>
2020-08-24 22:51:06 +0200

    Committer: Bryan van der net <bryan@pop-os.localdomain>  On branch master  Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-import fortiosapi
+import os
+from pprint import pprint
+from fortiosapi import FortiOSAPI
+
+FG = FortiOSAPI()
+
+# Source _host
+FG_HOST = '10.255.255.254'
+FG_USER = 'admin'
+FG_PASS = 'Judoka98'
+
+DEVICE = {
+    'host': FG_HOST,
+    'username': FG_USER,
+    'password': FG_PASS,
+}
+
+FG.login(**DEVICE)
+
+
+
+out = FG.get('system', 'global')
+
+pprint(out)
+
+FG.logout()
