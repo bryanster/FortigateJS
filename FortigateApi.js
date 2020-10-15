a987f576908287970a4ab78c8b7b200f0d56ec8a
bryan@nexezo.com <bryan@LTNEX01.localdomain>
2020-10-15 16:11:17 +0200

    added known functions

--- FortigateApi.js
+++ FortigateApi.js
@@ -6,6 +6,15 @@ class Api{
   }
   getPolicy(){return this._hello("cmdb/firewall/policy/")}
   getUser() {return this._hello("cmdb/user/local/")}
+  getAdmin(){return this._hello("cmdb/system/admin/")}
+  getVdom(){return this._hello("cmdb/system/vdom/")}
+  getSRoute(){return this._hello("cmdb/router/static/")}
+  getService(){return this._hello("cmdb/firewall.service/custom/")}
+  getServiceGroup(){return this._hello("cmdb/firewall.service/group/")}
+  getShaper(){return this._hello("cmdb/firewall.shaper/traffic-shaper/")}
+  getVip(){return this._hello("cmdb/firewall/vip/")}
+  getIpsecP1(){return this._hello("cmdb/vpn.ipsec/phase1-interface/")}
+  getIpsecP2(){return this._hello("cmdb/vpn.ipsec/phase2-interface/")}
   async _hello(url){
     var options = {
       uri: `https://${this.endpoint}/api/v2/${url}`,
