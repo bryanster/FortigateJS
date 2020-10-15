7674e15592a9e6a17548d1266b106d85ed15401b
bryan@nexezo.com <bryan@LTNEX01.localdomain>
2020-10-15 19:52:34 +0200

    created a back-up script in nodejs as example

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 var Yeet = require("./FortigateApi")
+var fs = require('fs');
+const { basename } = require("path");
 async function main(){
     var yeet = new Yeet("wf8jbbxGcnp9G7jf7Nw4s1QpmNq0fb", "firewall.nexezo.com")
     console.log(await yeet.getDeviceGlobal())
+    await writback(await yeet.getBackup())
 }
+
 main()
 
-
+function writback(config){
+    fs.writeFile('yeet.conf', config, 'utf8',(error)=>{
+        return
+    });
+    console.log("wrote to file")
+}
