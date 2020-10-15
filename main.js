67b65f9bafd0c7459a96626abf9f377fa76be3d8
bryan@nexezo.com <bryan@LTNEX01.localdomain>
2020-10-15 17:12:59 +0200

    removed old require

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-const Api = require("./FortigateApi")
 var Yeet = require("./FortigateApi")
-
-
 async function main(){
     var yeet = new Yeet("wf8jbbxGcnp9G7jf7Nw4s1QpmNq0fb", "firewall.nexezo.com")
     console.log(await yeet.getUser())
