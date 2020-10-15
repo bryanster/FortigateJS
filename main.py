2a5045fe23ebd29c3c2b9bf879016f6c1b42c345
bryan@nexezo.com <bryan@LTNEX01.localdomain>
2020-10-15 16:00:21 +0200

    deleted main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import requests
-endpoint = "https://firewall.nexezo.com/api/v2/cmdb/firewall/policy/"
-data = {"ip": "firewall.nexezo.com"}
-headers = {"Authorization": "Bearer wf8jbbxGcnp9G7jf7Nw4s1QpmNq0fb"}
-
-print(requests.get(endpoint, data=data, headers=headers).json())
