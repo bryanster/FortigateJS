22bc679e54e8691aa8e23b3796c84e633ae3c1c9
bryan@nexezo.com <bryan@LTNEX01.localdomain>
2020-10-15 17:58:47 +0200

    edit main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var Yeet = require("./FortigateApi")
 async function main(){
-    var yeet = new Yeet("wf8jbbxGcnp9G7jf7Nw4s1QpmNq0fb", "firewall.nexezo.com")
-    console.log(await yeet.getUpgradePath())
+    var yeet = new Yeet("wf8jbbxGcnp9G7jf7Nw4s1QpmNq0fb", "10.255.255.254")
+    console.log(await yeet.getVip())
 }
 main()
 
