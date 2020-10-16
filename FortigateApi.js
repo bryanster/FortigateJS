3847e37882961bc61e51dfe684c31b2bf3d4c0fd
bryan@nexezo.com <bryan@LTNEX01.localdomain>
2020-10-16 18:02:53 +0200

    create admin

--- FortigateApi.js
+++ FortigateApi.js
@@ -1,9 +1,11 @@
 const r = require('request-promise');
+const rp = require("request")
 class Api {
-  constructor(token, endpoint, Secure = true) {
+  constructor(token, endpoint, payload, Secure = true ) {
     this.token = token
     this.endpoint = endpoint
     this.Secure = Secure
+    this.payload = payload{}
   }
   getPolicy() { return this._get("cmdb/firewall/policy/") }
   getUser() { return this._get("cmdb/user/local/") }
@@ -34,6 +36,7 @@ class Api {
   getIntrusionPrevention() {return this._get("cmdb/ips/sensor")}
   getApplicationControl() {return this._get("cmdb/application/list")}
   getSslInspection()  {return this._get("cmdb/firewall/ssl-ssh-profile")}
+  createAdmin(username) {return this._post("cmdb/system/admin", `${username}`)}
   async _get(url, returnjson) {
     if (this.Secure == false) {
       var protocol = ("https://")
@@ -57,7 +60,26 @@ class Api {
       return result.results
     }
   }
+  async _post(url, username){var request = require('request');
+  var options = {
+    'method': 'POST',
+    'url': `https://${this.endpoint}/api/v2/${url}`,
+    'headers': {
+      'Content-Type': 'application/json;charset=UTF-8',
+      'Accept': 'application/json, text/plain, */*',
+      'X-Requested-With': 'XMLHttpRequest',
+      'Authorization': 'Bearer wf8jbbxGcnp9G7jf7Nw4s1QpmNq0fb'
+    },
+    body: `{\"name\":\"${username}\",\"q_origin_key\":\"\",\"wildcard\":\"disable\",\"remote-auth\":\"disable\",\"remote-group\":\"\",\"peer-auth\":\"disable\",\"peer-group\":\"\",\"trusthost1\":\"0.0.0.0/0\",\"trusthost2\":\"0.0.0.0/0\",\"trusthost3\":\"0.0.0.0/0\",\"trusthost4\":\"0.0.0.0/0\",\"trusthost5\":\"0.0.0.0/0\",\"trusthost6\":\"0.0.0.0/0\",\"trusthost7\":\"0.0.0.0/0\",\"trusthost8\":\"0.0.0.0/0\",\"trusthost9\":\"0.0.0.0/0\",\"trusthost10\":\"0.0.0.0/0\",\"ip6-trusthost1\":\"::/0\",\"ip6-trusthost2\":\"::/0\",\"ip6-trusthost3\":\"::/0\",\"ip6-trusthost4\":\"::/0\",\"ip6-trusthost5\":\"::/0\",\"ip6-trusthost6\":\"::/0\",\"ip6-trusthost7\":\"::/0\",\"ip6-trusthost8\":\"::/0\",\"ip6-trusthost9\":\"::/0\",\"ip6-trusthost10\":\"::/0\",\"accprofile\":{\"name\":\"prof_admin\",\"q_origin_key\":\"prof_admin\",\"comments\":\"\",\"secfabgrp\":\"read-write\",\"ftviewgrp\":\"read-write\",\"authgrp\":\"read-write\",\"sysgrp\":\"read-write\",\"netgrp\":\"read-write\",\"loggrp\":\"read-write\",\"fwgrp\":\"read-write\",\"vpngrp\":\"read-write\",\"utmgrp\":\"read-write\",\"wifi\":\"read-write\",\"admintimeout-override\":\"disable\",\"q_ref\":0,\"q_static\":true,\"q_no_rename\":false,\"q_global_entry\":false,\"q_type\":10,\"q_path\":\"system\",\"q_name\":\"accprofile\",\"q_mkey_type\":\"string\",\"q_no_edit\":false},\"allow-remove-admin-session\":\"enable\",\"comments\":\"\",\"vdom\":[{\"name\":\"root\",\"q_origin_key\":\"root\",\"datasource\":\"system.vdom\"}],\"schedule\":\"\",\"accprofile-override\":\"disable\",\"radius-vdom-override\":\"disable\",\"password-expire\":\"0000-00-00 00:00:00\",\"force-password-change\":\"disable\",\"gui-dashboard\":[],\"two-factor\":\"disable\",\"fortitoken\":\"\",\"email-to\":\"\",\"sms-server\":\"fortiguard\",\"sms-custom-server\":\"\",\"sms-phone\":\"\",\"guest-auth\":\"disable\",\"guest-usergroups\":[],\"guest-lang\":\"\",\"login-time\":[],\"gui-global-menu-favorites\":[],\"gui-vdom-menu-favorites\":[],\"gui-new-feature-acknowledge\":[],\"currentGuestLanguage\":\"Default Language (English)\",\"type\":\"local\",\"restrictTrusted\":false,\"tfaVisible\":true,\"password\":\"Welkom01\",\"confirmPassword\":\"Welkom01\"}`
+  
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
+  });
+  }
 }
+
 
 module.exports = Api
 
