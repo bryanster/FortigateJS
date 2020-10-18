32bdc79af6c62e6c90d32f1160f202f68f7f1843
bryan@nexezo.com <bryan@LTNEX01.localdomain>
2020-10-18 22:33:05 +0200

    reboot schaduler example

--- Examples/rebootSchaduler.js
+++ Examples/rebootSchaduler.js
@@ -5,3 +5,5 @@ var yeet = new fortigateapi("wf8jbbxGcnp9G7jf7Nw4s1QpmNq0fb", "firewall.nexezo.c
 cron.schedule('*/5 * * * *', () => {
     yeet.reboot();
   });
+// cron tab like time entry 
+// above is every 5 minutes
