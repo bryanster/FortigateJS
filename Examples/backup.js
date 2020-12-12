cd9869a2b150f7bd3868df6e31b17ae4a07ee304
bryanster <45668775+bryanster@users.noreply.github.com>
2020-12-12 13:28:27 +0100

    Update backup.js

--- Examples/backup.js
+++ Examples/backup.js
@@ -1,17 +1,13 @@
-var Yeet = require("./FortigateApi")
-var fs = require('fs');
-const { basename } = require("path");
+var fortigateapi = require("./FortigateApi")
+var fs = require("fs").promises
+var fortigate = new fortigateapi("wf8jbbxGcnp9G7jf7Nw4s1QpmNq0fb", "192.168.2.240")
+
 async function main(){
     
-    var yeet = new Yeet("wf8jbbxGcnp9G7jf7Nw4s1QpmNq0fb", "firewall.nexezo.com") // APi key , endpoint
-    await writback(await yeet.getBackup())
+    var download = await fortigate.getBackup("test.conf"); //the parameter is the output file name
+    console.log(download)//this will return true on success
+    
+   
 }
 
 main()
-
-// function writback(config){
-//     fs.writeFile('yeet.conf', config, 'utf8',(error)=>{
-//         return
-//     });
-//     console.log("wrote config to yeet.conf")
-// }
