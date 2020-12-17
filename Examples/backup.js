b22a875e0d50e990a7d3b77c6a4357a82729b4cf
bryanster <you@example.com>
2020-12-18 00:10:57 +0100

    refined

--- Examples/backup.js
+++ Examples/backup.js
@@ -1,11 +1,17 @@
-var fortigateapi = require("./FortigateApi")
+var fortigateapi = require("../FortigateApi")
 var fs = require("fs").promises
 var fortigate = new fortigateapi("wf8jbbxGcnp9G7jf7Nw4s1QpmNq0fb", "192.168.2.240")
 
 async function main(){
     
     var download = await fortigate.getBackup("test.conf"); //the parameter is the output file name
-    console.log(download)//this will return true on success
+    if(download == true){
+        console.log('download successfull')
+    }
+    else{
+        console.log('download failed')
+    }
+
     
    
 }
